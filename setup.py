2c3cd7afddf9ebbc504d794155ada0c7a93e5bf4
Ijtaba Hasan <iijtaba.hasan@gmail.com>
2026-01-15 09:52:42 +0500

    Mongo db data add

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="VisaApprovel",  # your project name
+    version="0.0.0",
+    author="Ijtaba",
+    author_email="iijtaba.hasan@gmail.com",
+    packages=find_packages(),
+    
+)
